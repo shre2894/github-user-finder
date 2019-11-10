662e6c1773a0e14d7046291f9e99e02a6c5d9a62
shre2894 <30913331+shre2894@users.noreply.github.com>
2019-11-11 00:58:42 +0530

    added axios & fetching users from github

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "./styles.css";
 import Navbar from "./layout/Navbar";
 import Users from "./users/Users";
 
 class App extends React.Component {
+  state = {
+    loading: false,
+    users:[]
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get("https://api.github.com/users")
+    this.setState({users:res.data,loading:false})
+  }
+
   render() {
+    const {loading,users}= this.state
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={loading} users={users } />
         </div>
       </div>
     );
--- src/users/Users.js
+++ src/users/Users.js
@@ -2,20 +2,11 @@ import React from "react";
 import UserItem from "./UserItem";
 
 class Users extends React.Component {
-  state = {
-    users: [
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      }
-    ]
-  };
+
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => {
+        {this.props.users.map(user => {
           return <UserItem key={user.id} user={user} />;
         })}
       </div>
