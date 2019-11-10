ff29f02f3decf6787bd018d94c56ffb3c9f13b02
shre2894 <30913331+shre2894@users.noreply.github.com>
2019-11-11 01:11:40 +0530

    added spinner on fetching data

--- src/users/Users.js
+++ src/users/Users.js
@@ -1,18 +1,26 @@
 import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import propTypes from "prop-types";
 
-class Users extends React.Component {
-
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => {
-          return <UserItem key={user.id} user={user} />;
-        })}
-      </div>
-    );
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
   }
-}
+
+  return (
+    <div style={userStyle}>
+      {users.map(user => ( 
+         <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+Users.propTypes = {
+  users: propTypes.array.isRequired,
+  loading: propTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: "grid",
