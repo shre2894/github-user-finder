d5b2ad26a29f556f7a803c755288fcd6ec3b4db9
shreyanshj <shreyanshj@provexam.com>
2019-11-15 00:30:03 +0530

    added api of github oauth

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import "./styles.css";
-import Navbar from "./layout/Navbar";
-import Users from "./users/Users";
+import './styles.css';
+import Navbar from './layout/Navbar';
+import Users from './users/Users';
 
 class App extends React.Component {
-  state = {
-    loading: false,
-    users:[]
-  }
+	state = {
+		loading: false,
+		users: []
+	};
 
-  async componentDidMount() {
-    this.setState({loading: true});
-    const res = await axios.get("https://api.github.com/users")
-    this.setState({users:res.data,loading:false})
-  }
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users?client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ users: res.data, loading: false });
+	}
 
-  render() {
-    const {loading,users}= this.state
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Users loading={loading} users={users } />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { loading, users } = this.state;
+		return (
+			<div className="App">
+				<Navbar />
+				<div className="container">
+					<Users loading={loading} users={users} />
+				</div>
+			</div>
+		);
+	}
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
